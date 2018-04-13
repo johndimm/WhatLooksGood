f1ae0172d39a06dee740751e1c89e708f7fb326e
John Dimm <jdimm@yahoo.com>
2018-04-13 08:58:12 -0700

    Do not require 2 photos with exact caption from the same restaurant.

--- db/total_caption.sql
+++ db/total_caption.sql
@@ -9,6 +9,7 @@ select
   replace(replace(caption, '\r\n',''), '\n','') as caption
   , sum(cnt) as num_photos
   , count(*) as cnt
+  , max(cnt) as inside_cnt
 from
 (
     select b.name, caption, count(*) as cnt
@@ -18,7 +19,7 @@ from
     and caption not in ('', ':)')
     and locate('://', caption) = 0
     group by b.name, caption
-    having cnt > 1
+    # having cnt > 1
     order by cnt desc
 ) as t
 where length(caption) - length(replace(caption, ' ','')) < 3
@@ -27,16 +28,6 @@ having cnt > 1
 order by cnt desc
 ;
 
-#
-# Step 2: Add other captions that contain the core dishes. (not used )
-#
-drop table if exists total_caption_ex;
-create table total_caption_ex as
-select wc.caption as food_item, photo.id as photo_id, photo.caption
-from yelp_db.photo
-join total_caption as wc on locate(wc.caption, photo.caption) > 0
-order by food_item, length(photo.caption)
-;
 
 select *
 from total_caption
