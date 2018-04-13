1a825ebc288f386cb114d87137c3c2483f4401c8
John Dimm <jdimm@yahoo.com>
2018-04-13 08:57:40 -0700

    Added sample_photo and recommendations

--- db/run.sh
+++ db/run.sh
@@ -1,9 +1,9 @@
 # Dump yelp photo captions.
-mysql yelp_db < get_captions.sql > captions.txt
+#mysql yelp_db < get_captions.sql > captions.txt
 
 # Grab the two tables we need from the yelp dataset challenge.
-mysqldump yelp_db business photo > business_photo.sql
-mysql < business_photo.sql
+#mysqldump yelp_db business photo > business_photo.sql
+#mysql < business_photo.sql
 
 # Exact match of the complete caption.
 mysql < total_caption.sql
@@ -20,3 +20,9 @@ mysql < dish.sql
 
 # Stored procedures for retrieval.
 mysql < reco.sql
+
+# Get a sample dish for each business.
+mysql < sample_photo.sql
+
+# Generate recommendations tables.
+mysql < recommendations.sql
