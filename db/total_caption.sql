6f9e3dea0a383c4b628b0d0223232cdcc5f422e0
John Dimm <jdimm@yahoo.com>
2018-04-30 10:06:25 -0700

    Clean captions.

--- db/total_caption.sql
+++ db/total_caption.sql
@@ -3,27 +3,37 @@
 #
 # Step 1: look for captions that occur multiple times
 #
+
+drop function if exists cleanDish;
+delimiter //
+create function cleanDish(dish varchar(255))
+returns varchar(255)
+begin
+  return replace(replace(replace(replace(trim(dish),'.',''),'!',''), '\r\n',''),'\n','');
+end //
+delimiter ;
+
 drop table if exists total_caption;
 create table total_caption as 
 select  
-  replace(replace(caption, '\r\n',''), '\n','') as caption
+  clean_caption as caption
   , sum(cnt) as num_photos
   , count(*) as cnt
   , max(cnt) as inside_cnt
 from
 (
-    select b.name, caption, count(*) as cnt
+    select b.name, cleanDish(caption) as clean_caption, count(*) as cnt
     from yelp_db.photo
     join yelp_db.business as b on b.id=photo.business_id
     where label = 'food'
     and caption not in ('', ':)')
     and locate('://', caption) = 0
-    group by b.name, caption
+    group by b.name, clean_caption
     # having cnt > 1
     order by cnt desc
 ) as t
-where length(caption) - length(replace(caption, ' ','')) < 3
-group by caption
+where length(clean_caption) - length(replace(clean_caption, ' ','')) < 3
+group by clean_caption
 having cnt > 1
 order by cnt desc
 ;
