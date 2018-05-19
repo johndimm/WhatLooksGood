16b5e4e92887c15ab20cd3c1118a15ae36abcbde
John Dimm <jdimm@yahoo.com>
2018-05-18 17:47:20 -0700

    Removed unused login credentials.

--- app/get.php
+++ app/get.php
@@ -14,25 +14,6 @@ function printJSON($db, $sql) {
   echo json_encode($result);
 }
 
-function xdbInit() {
-    $mysql_host = "localhost";
-    $mysql_user = "johndimm_mysql";
-    $mysql_password = "Jfkdls92";
-    $mysql_database = "johndimm_olympics";
-
-    $mysql_host = "localhost";
-    $mysql_user = "accuscore";
-    $mysql_password = "accuscore2drive";
-    $mysql_database = "yelp_db";
-
-    // Connecting, selecting database
-    $db = mysql_connect($mysql_host, $mysql_user, $mysql_password)
-        or die('Could not connect: ' . mysql_error());
-    mysql_select_db($mysql_database) or die('Could not select database');
-    mysql_set_charset('utf8',$db);
-    return $db;
-}
-
 function main() {
 //  echo '[{"url":"http://www.johndimm.com/photos/2014/10/IMG_20141019_133156.jpg", "caption":"", "rating":"good", "rotation":0}]';
 //  return;
