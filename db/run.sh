af8f85d70f1b6d3f65d303971523f356caccadee
John Dimm <jdimm@yahoo.com>
2018-05-20 17:04:43 -0700

    build.sh and import_yelp.sh created.

--- db/run.sh
+++ db/run.sh
@@ -1,38 +1,8 @@
-# Dump yelp photo captions.
-#mysql yelp_db < get_captions.sql > captions.txt
-
-# Grab the two tables we need from the yelp dataset challenge.
-#mysqldump yelp_db business photo > business_photo.sql
-#mysql < business_photo.sql
-
-# Exact match of the complete caption.
-mysql < total_caption.sql
-
-# Extract noun phrases.
-python phrases.py captions.txt > words.txt
-mysql < load.sql
-
-# Find phrases that are the only phrase in many captions.
-mysql < singleton_phrase.sql
-
 # Create tables.
 mysql < tables.sql
 
-# Create dish, count tables.
-mysql < dish.sql
+# Import from yelp captions.
+./import_yelp.sh
 
-# Generate some counts.
-mysql < count.sql
-
-# Stored procedures for retrieval.
-mysql < reco.sql
-
-# Get a sample dish for each business.
-mysql < sample_photo.sql
-
-# Generate recommendations tables.
-mysql < dish_reco.sql
-mysql < business_reco.sql
-
-# Create the list of dishes for the home page.
-mysql -e "call gen_dish_sample()";
+# Create count tables.
+./build.sh
